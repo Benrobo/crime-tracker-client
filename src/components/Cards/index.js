ce03eeb5762135ea1e2a3dde4f48d27e9d9aad86
Benrobo <alumonabenaiah71@gmail.com>
2022-03-04 21:51:31 -0800

    added deleting of users account

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,35 +1,124 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 
 import "./style.css"
 import { FiMoreVertical } from 'react-icons/fi'
 import img from "../../assets/img/police.png"
+import DataContext from '../../context/DataContext'
+import apiRoutes from '../../api_routes'
+import { Util, Notification } from '../../helpers/util'
 
-export function UserCards({ setToggleAction, toggleAction }) {
+const util = new Util();
+const notif = new Notification();
+
+export function UserCards({ setToggleAction, toggleAction, id, name, mail, rank, role, date }) {
+
+    const { localData } = useContext(DataContext)
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [data, setData] = useState([]);
+
+    function showMoreAction(e) {
+        let parent = e.target.parentElement;
+        if (parent.className === "top") {
+            let child = parent.querySelector(".more-cont");
+            child.classList.toggle("show")
+        }
+    }
+
+    function hideElem(e) {
+        let parent = e.target.parentElement;
+        parent.style.display = "none"
+    }
+
+    async function deleteOfficerAccount(officerId) {
+        if (officerId === "") {
+            return notif.error("officer id is empty")
+        }
+        let confirm;
+
+        if (officerId === localData.id) {
+            confirm = window.confirm("Are you sure you wanna delete your account?")
+        }
+        confirm = window.confirm("Are you sure you wanna delete this officer account?")
+
+        if (confirm) {
+
+            let options, url;
+            try {
+                url = apiRoutes.deleteOfficer;
+                options = {
+                    method: "delete",
+                    headers: {
+                        Authorization: `Bearer ${localData.refreshToken}`,
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        userId: localData.id,
+                        officerId
+                    })
+                };
+                setLoading(true);
+                let res = await fetch(url, options);
+                let data = await res.json();
+
+                if (data && data.error === true) {
+                    console.error(data.message);
+                    return notif.error(data.message);
+                }
+
+                setLoading(false);
+                notif.success(data.message)
+                setTimeout(() => {
+                    window.location.reload()
+                }, 1200);
+            } catch (err) {
+                setLoading(false);
+                setError(err.message);
+                notif.error(err.message);
+            }
+
+            return
+        }
+    }
 
     return (
         <div className="user-box">
             <div className="top">
-                <img src={img} alt="" className="img" />
-                {toggleAction && <div className="more-cont">
-                    <li>Delete</li>
-                </div>}
+                <img src={navigator.connection.downlink > 1 ? `https://avatars.dicebear.com/api/micah/${name}.svg` : img} alt="" className="img" />
+                <div className="more-cont">
+                    <li data-id={id} onClick={async (e) => {
+                        let target = e.target.dataset.id;
+                        await deleteOfficerAccount(target)
+                        // hideElem(e)
+                    }}>{loading ? "Deleting account" : "Delete"}</li>
+                </div>
                 <br />
-                <FiMoreVertical className="icon" onClick={() => setToggleAction(!toggleAction)} />
-                <p className="name">John Doe</p>
-                <small>john@mail.com</small>
+                {
+                    localData.role === "admin" ?
+                        <FiMoreVertical className="icon" onClick={(e) => {
+                            showMoreAction(e)
+                            setToggleAction(!toggleAction)
+                        }} />
+                        :
+                        localData.id === id ?
+                            <FiMoreVertical className="icon" onClick={(e) => {
+                                showMoreAction(e)
+                                setToggleAction(!toggleAction)
+                            }} />
+                            :
+                            ""
+                }
+                <p className="name">{name} {id === localData.id ? (<kbd>you</kbd>) : (<kbd>others</kbd>)}</p>
+                <small>{mail}</small>
                 <div className="officer-type">
                     <div className="left">Type</div>
-                    <div className="right">Inspector General</div>
+                    <div className="right">{rank.toUpperCase()}</div>
                 </div>
             </div>
             <div className="bottom">
                 <div className="box">
-                    <small>Cases</small>
-                    <h6>6</h6>
-                </div>
-                <div className="box">
                     <small>Member since</small>
-                    <h6>Feb 2, 2020</h6>
+                    <h6>{date.split(",")[0]}</h6>
                 </div>
             </div>
         </div>
--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import MainCont from '../../components/MainCont/MainCont'
 import Profile from '../../components/ProfileBar/Profile'
 import LeftNavbar from '../../components/LeftNavbar'
@@ -6,10 +6,53 @@ import LeftNavbar from '../../components/LeftNavbar'
 import "./style.css"
 import Layout from '../../components/Layout/Layout'
 import { UserCards } from '../../components/Cards'
+import DataContext from '../../context/DataContext'
+import apiRoutes from '../../api_routes'
+import { Util, Notification } from '../../helpers/util'
+
+const util = new Util();
+const notif = new Notification();
 
 function Users() {
+    const { localData } = useContext(DataContext);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [data, setData] = useState([]);
 
     const [toggleAction, setToggleAction] = useState(false)
+
+    // fetch all users
+    useEffect(() => {
+        (async function getOfficers() {
+            let options, url;
+            try {
+                url = apiRoutes.getOfficers;
+                options = {
+                    method: "post",
+                    headers: {
+                        Authorization: `Bearer ${localData.refreshToken}`,
+                        "content-type": "application/json",
+                    },
+                };
+                setLoading(true);
+                let res = await fetch(url, options);
+                let data = await res.json();
+
+                if (data && data.error === true) {
+                    console.error(data.message);
+                    setError(data.message);
+                    return notif.error(data.message);
+                }
+
+                setLoading(false);
+                setData(data.data);
+            } catch (err) {
+                setLoading(false);
+                setError(err.message);
+                notif.error(err.message);
+            }
+        })();
+    }, []);
 
     return (
         <Layout>
@@ -22,7 +65,31 @@ function Users() {
                 <br />
 
                 <div className="users-cards-cont">
-                    <UserCards setToggleAction={setToggleAction} toggleAction={toggleAction} />
+                    {
+                        loading ?
+                            <p>fetching users....</p>
+                            :
+                            error !== "" ?
+                                <p>{error}</p>
+                                :
+                                data && data.length === 0 ?
+                                    <p>No officers were found</p>
+                                    :
+                                    data.map((users, i) => {
+                                        return (
+
+                                            <UserCards setToggleAction={setToggleAction} toggleAction={toggleAction}
+                                                key={i}
+                                                id={users.userId}
+                                                name={users.userName}
+                                                mail={users.mail}
+                                                rank={users.userRank}
+                                                role={users.userRole}
+                                                date={users.joined}
+                                            />
+                                        )
+                                    })
+                    }
                 </div>
             </MainCont>
             <Profile />
