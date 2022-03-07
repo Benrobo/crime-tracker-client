0e132d83c5a04327de67cd2a5cd20a150051e003
Benrobo <alumonabenaiah71@gmail.com>
2022-03-07 11:08:47 -0800

    updated evidence logic

--- src/pages/Evidence/AddEvidence.js
+++ src/pages/Evidence/AddEvidence.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import MainCont from '../../components/MainCont/MainCont'
 import Profile from '../../components/ProfileBar/Profile'
 import LeftNavbar from '../../components/LeftNavbar'
 
 import "./style.css"
 import Layout from '../../components/Layout/Layout'
+import apiRoutes from "../../api_routes"
+import DataContext from "../../context/DataContext"
+import { Util, Notification } from "../../helpers/util"
 
+const util = new Util()
+const notif = new Notification(4000)
 
 function AddEvidence() {
     return (
@@ -24,6 +29,138 @@ export default AddEvidence
 
 
 function AddEvidenceForm() {
+    const { localData } = useContext(DataContext)
+    const [loadingCase, setCaseLoading] = useState(false);
+    const [loadingEvidence, setEvidenceLoading] = useState(false);
+    const [loadingsuspects, setSuspectLoading] = useState(false);
+    const [casesData, setCasesData] = useState([])
+    const [suspectsdata, setSuspectsData] = useState([])
+
+    // form fields state
+    const [caseId, setCaseId] = useState("");
+    const [evidence, setEvidence] = useState("")
+    const [suspectName, setSuspectName] = useState("")
+    const [note, setNote] = useState("")
+    const [suspectId, setSuspectId] = useState("")
+
+
+    // get cases data
+    useEffect(() => {
+        (async () => {
+            // get cases data
+            try {
+                let url = apiRoutes.getCases;
+                let options = {
+                    method: "post",
+                    headers: {
+                        "Authorization": `Bearer ${localData.refreshToken}`,
+                        "content-type": "application/json"
+                    }
+                }
+                setCaseLoading(true)
+                let res = await fetch(url, options);
+                let data = await res.json();
+                console.log(data);
+                if (data && data.error === true) {
+                    console.error(data.message);
+                    return notif.error(data.message)
+                }
+                setCaseLoading(false);
+                setCasesData(data.data)
+
+            } catch (err) {
+                setCaseLoading(false);
+                notif.error(err.message)
+            }
+        })()
+    }, [])
+    // get suspectdata
+    useEffect(() => {
+        (async () => {
+            try {
+                let url = apiRoutes.allSuspects;
+                let options = {
+                    method: "post",
+                    headers: {
+                        "Authorization": `Bearer ${localData.refreshToken}`,
+                        "content-type": "application/json"
+                    }
+                }
+                setSuspectLoading(true)
+                let res = await fetch(url, options);
+                let data = await res.json();
+                console.log(data);
+                if (data && data.error === true) {
+                    return notif.error(data.message)
+                }
+                setSuspectLoading(false);
+                setSuspectsData(data.data)
+
+            } catch (err) {
+                setSuspectLoading(false);
+                notif.error(err.message)
+            }
+        })()
+    }, [])
+
+    function getSuspectName(id) {
+        let name = suspectsdata.filter((suspect) => {
+            return suspect.id === id
+        })[0].suspectName
+        setSuspectName(name)
+    }
+
+    async function addEvidence() {
+        if (caseId === "") {
+            return notif.error("caseid cant be empty")
+        };
+        if (evidence === "") {
+            return notif.error("evidence cant be empty")
+        }
+        if (note === "") {
+            return notif.error("note cant be empty")
+        }
+        if (suspectName === "") {
+            return notif.error("suspectName cant be empty")
+        }
+        if (suspectId === "") {
+            return notif.error("suspectId cant be empty")
+        }
+
+        try {
+            let url = apiRoutes.addEvidence;
+            let options = {
+                method: "post",
+                headers: {
+                    "Authorization": `Bearer ${localData.refreshToken}`,
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    userId: localData.id,
+                    caseId,
+                    suspectName,
+                    evidence,
+                    suspectId,
+                    note
+                })
+            }
+            setEvidenceLoading(true)
+            let res = await fetch(url, options);
+            let data = await res.json();
+            console.log(data);
+            if (data && data.error === true) {
+                setEvidenceLoading(false)
+                return notif.error(data.message)
+            }
+
+            setEvidenceLoading(false);
+            notif.success(data.message)
+        } catch (err) {
+            setEvidenceLoading(false);
+            notif.error(err.message)
+        }
+
+    }
 
     return (
         <div className="addevidence-cont">
@@ -34,29 +171,61 @@ function AddEvidenceForm() {
             <div className="form-cont">
                 <div className="box">
                     <label htmlFor="">Case ID</label>
-                    <select className="select">
-                        <option value=""> --- case id ---</option>
+                    <select name="" id="" className="select" onChange={(e) => {
+                        setCaseId(e.target.value)
+                    }}>
+                        <option value="">{loadingCase ? "loading..." : "-- case id ---"}</option>
+                        {
+                            loadingCase ?
+                                <option value="">Loading...</option>
+                                :
+                                casesData && casesData.length === 0 ?
+                                    <option value="">No cases</option>
+                                    :
+                                    casesData.map((list) => {
+                                        return (
+                                            <option key={list.id} value={list.id}>{list.id}</option>
+                                        )
+                                    })
+                        }
                     </select>
                 </div>
                 <div className="box">
                     <label htmlFor="">Evidence</label>
-                    <input type="text" placeholder="Knife and bottle" defaultValue={"Knife and Bottle"} className="input" />
+                    <input type="text" placeholder="Knife and bottle" defaultValue={evidence} onChange={(e) => setEvidence(e.target.value)} className="input" />
                 </div>
                 <div className="box">
                     <label htmlFor="">Suspect Name</label>
-                    <select className="select">
-                        <option value=""> --- suspect name---</option>
-                        <option value="">husband wife</option>
+                    <select name="" id="" className="select" onChange={(e) => {
+                        setSuspectId(e.target.value)
+                        getSuspectName(e.target.value)
+                    }}>
+                        <option value="">{loadingsuspects ? "loading..." : "-- suspects name ---"}</option>
+                        {
+                            loadingsuspects ?
+                                <option value="">Loading...</option>
+                                :
+                                suspectsdata && suspectsdata.length === 0 ?
+                                    <option value="">No suspects</option>
+                                    :
+                                    suspectsdata.map((list) => {
+                                        return (
+                                            <option key={list.id} value={list.id}>{list.suspectName}</option>
+                                        )
+                                    })
+                        }
                     </select>
                 </div>
                 <div className="box">
                     <label htmlFor="">Note</label>
-                    <textarea cols="30" rows="2" defaultValue={"some note here"} className="note"></textarea>
+                    <textarea cols="30" rows="2" defaultValue={note} onChange={(e) => setNote(e.target.value)} className="note"></textarea>
                 </div>
                 <hr />
                 <div className="action">
                     <button className="cancel btn">Cancel</button>
-                    <button className="add btn">Add Evidence</button>
+                    <button className="add btn" onClick={() => addEvidence()}>
+                        {loadingEvidence ? "Adding evidence..." : "Add Evidence"}
+                    </button>
                 </div>
             </div>
         </div>
--- src/pages/Evidence/Evidence.js
+++ src/pages/Evidence/Evidence.js
@@ -1,14 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import MainCont from '../../components/MainCont/MainCont'
 import Profile from '../../components/ProfileBar/Profile'
 import LeftNavbar from '../../components/LeftNavbar'
 
 import "./style.css"
 import Layout from '../../components/Layout/Layout'
-import { BsTrashFill } from 'react-icons/bs'
+import { BsPen, BsPencilFill, BsTrashFill } from 'react-icons/bs'
+import apiRoutes from "../../api_routes"
+import DataContext from "../../context/DataContext"
+import { Util, Notification } from "../../helpers/util"
+
+const util = new Util()
+const notif = new Notification()
 
 
 function Evidence() {
+    const { localData } = useContext(DataContext)
+    const [loadingCase, setCaseLoading] = useState(false);
+    const [loadingEvidence, setEvidenceLoading] = useState(false);
+    const [error, setError] = useState("")
+    const [casesData, setCasesData] = useState([])
+    const [evidenceData, setEvidenceData] = useState([])
+    const [caseid, setCaseId] = useState("");
+
+    useEffect(() => {
+        (async () => {
+            // get cases data
+            try {
+                let url = apiRoutes.getCases;
+                let options = {
+                    method: "post",
+                    headers: {
+                        "Authorization": `Bearer ${localData.refreshToken}`,
+                        "content-type": "application/json"
+                    }
+                }
+                setCaseLoading(true)
+                let res = await fetch(url, options);
+                let data = await res.json();
+
+                if (data && data.error === true) {
+                    console.error(data.message);
+                    return notif.error(data.message)
+                }
+                setCaseLoading(false);
+                setCasesData(data.data)
+
+            } catch (err) {
+                setCaseLoading(false);
+                notif.error(err.message)
+            }
+        })()
+    }, [])
+
+    async function FetchEvidence(caseId) {
+        if (caseId === "") return;
+
+        try {
+            let url = apiRoutes.getEvidenceId;
+            let options = {
+                method: "post",
+                headers: {
+                    "Authorization": `Bearer ${localData.refreshToken}`,
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({ userId: localData.id, caseId })
+            }
+            setEvidenceLoading(true)
+            let res = await fetch(url, options);
+            let data = await res.json();
+            console.log(data);
+            if (data && data.error === true) {
+                console.error(data.message);
+                return notif.error(data.message)
+            }
+
+            setEvidenceLoading(false);
+            setEvidenceData(data.data)
+
+        } catch (err) {
+            setEvidenceLoading(false);
+            notif.error(err.message)
+        }
+
+    }
+
     return (
         <Layout>
             <LeftNavbar active="evidence" />
@@ -18,12 +94,28 @@ function Evidence() {
                 <div className="evidence-top-head">
                     <div className="bx">
                         <label htmlFor="">Case ID</label>
-                        <select name="" id="" className="select">
-                            <option value="">-- case id ---</option>
+                        <select name="" id="" className="select" onChange={(e) => {
+                            setCaseId(e.target.value)
+                            FetchEvidence(e.target.value)
+                        }}>
+                            <option value="">{loadingCase ? "loading..." : "-- case id ---"}</option>
+                            {
+                                loadingCase ?
+                                    <option value="">Loading...</option>
+                                    :
+                                    casesData && casesData.length === 0 ?
+                                        <option value="">No cases</option>
+                                        :
+                                        casesData.map((list) => {
+                                            return (
+                                                <option key={list.id} value={list.id}>{list.id}</option>
+                                            )
+                                        })
+                            }
                         </select>
                     </div>
                     <br />
-                    <EvidenceTable />
+                    <EvidenceTable data={evidenceData} loading={loadingEvidence} />
                 </div>
             </MainCont>
             <Profile />
@@ -34,7 +126,8 @@ function Evidence() {
 export default Evidence
 
 
-function EvidenceTable() {
+function EvidenceTable({ data, loading }) {
+    const { localData } = useContext(DataContext)
 
     return (
         <div className="evidence-cont mt-5">
@@ -53,39 +146,53 @@ function EvidenceTable() {
                         <th>Case Id</th>
                         <th>Evidence</th>
                         <th>Suspect Name</th>
+                        <th>Relation</th>
                         <th>Note</th>
                         <th>Rank</th>
                         <th>Officer Name</th>
                         <th>Action</th>
                     </thead>
                     <tbody>
-                        {Array.from("123vfvv").map((_, i) => {
-                            return (
-                                <tr className="mt-3" key={i}>
-                                    <td>
-                                        <small>sdsdf43fvfdv</small>
-                                    </td>
-                                    <td>
-                                        <small>Knife and Bottle</small>
-                                    </td>
-                                    <td>
-                                        <small>Husband Wife</small>
-                                    </td>
-                                    <td className="status">
-                                        <small>Badass Guy</small>
-                                    </td>
-                                    <td>
-                                        <small>7</small>
-                                    </td>
-                                    <td>
-                                        <small>John Doe</small>
-                                    </td>
-                                    <td className="action">
-                                        <BsTrashFill className="icon" />
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                        {loading === false ?
+                            data && data.length > 0 ?
+                                data.map((list, i) => {
+                                    return (
+                                        <tr className="mt-3" key={i}>
+                                            <td>
+                                                <small>{list.caseId}</small>
+                                            </td>
+                                            <td>
+                                                <small>{list.evidence}</small>
+                                            </td>
+                                            <td>
+                                                <small>{list.relation}</small>
+                                            </td>
+                                            <td>
+                                                <small>{list.suspectName}</small>
+                                            </td>
+                                            <td className="status">
+                                                <small>{list.note}</small>
+                                            </td>
+                                            <td>
+                                                <small>{list.rank}</small>
+                                            </td>
+                                            <td>
+                                                <small>{list.userName}</small>
+                                            </td>
+                                            {list.userId === localData.id ? <td className="action">
+                                                <BsTrashFill className="icon delete" data-evidenceId={list.id} data-caseId={list.caseId} />
+                                                <BsPencilFill className="icon edit" data-evidenceId={list.id} data-caseId={list.caseId} />
+                                            </td> : <td><kbd>none</kbd></td>}
+                                        </tr>
+                                    )
+                                })
+                                :
+                                <small>Evidence is empty</small>
+                            :
+                            <tr>
+                                <td>loading...</td>
+                            </tr>
+                        }
                     </tbody>
                 </table>
             </div>
